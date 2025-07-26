a731e5364c906a989598978e60d19399fb329c8d
Mohebullah Yousofi <mohebullah.yousofi@gmail.com>
2025-07-26 15:17:45 +0430

    move all routes to routes folder and and active ES module

---
+++ routes/posts.js
@@ -0,0 +1,44 @@
+import express from 'express';
+const router = express.Router();
+
+let posts = [
+   {id:1, title: 'post One'},
+   {id:2, title: 'post Two'},
+   {id:3, title: 'post Three'},
+];
+
+// Get all posts
+router.get('/', (req, res)=>{
+    //working with query
+    const limit = parseInt(req.query.limit);
+    if(!isNaN(limit) && limit > 0){
+        res.status(200).json(posts.slice(0, limit));
+    }
+    res.status(200).json(posts);
+});
+
+// Get single posts
+router.get('/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post){
+     return res.status(404).json({msg: `A post with the id of ${id} was not found`});
+    }
+    res.status(200).json(post);
+    
+});
+
+/*
+app.get('/', (req, res) => {
+    // res.send('Hello World');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    // res.send('About');
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+*/
+
+export default router;
--- server.js
+++ server.js
@@ -1,50 +1,14 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
 const port = process.env.PORT || 8000;
 const app = express();
 
 //setup static folder
 // app.use(express.static(path.join(__dirname, 'public')));
 
-let posts = [
-   {id:1, title: 'post One'},
-   {id:2, title: 'post Two'},
-   {id:3, title: 'post Three'},
-];
-
-// Get all posts
-app.get('/api/posts', (req, res)=>{
-    //working with query
-    const limit = parseInt(req.query.limit);
-    if(!isNaN(limit) && limit > 0){
-        res.status(200).json(posts.slice(0, limit));
-    }
-    res.status(200).json(posts);
-});
-
-// Get single posts
-app.get('/api/posts/:id', (req, res)=>{
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-
-    if(!post){
-     return res.status(404).json({msg: `A post with the id of ${id} was not found`});
-    }
-    res.status(200).json(post);
-    
-});
-
-/*
-app.get('/', (req, res) => {
-    // res.send('Hello World');
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/about', (req, res) => {
-    // res.send('About');
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});
-*/
+//Routes
+app.use('/api/posts',posts);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
