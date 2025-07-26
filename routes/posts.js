4405499cbd696509e1b6a61087c330b188db55f5
Mohebullah Yousofi <mohebullah.yousofi@gmail.com>
2025-07-26 17:10:30 +0430

    post update added

--- routes/posts.js
+++ routes/posts.js
@@ -44,7 +44,18 @@ router.post('/',(req, res)=>{
     res.status(201).json(posts);
 });
 
+// Update Post
+router.put('/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    const post = posts.find((post)=>post.id === id);
+    
+    if(!post){
+     return res.status(404).json({msg: `A post with the id of ${id} was not found`});
+    }
 
+    post.title = req.body.title;
+    res.status(200).json(posts);
+});
 /*
 app.get('/', (req, res) => {
     // res.send('Hello World');
