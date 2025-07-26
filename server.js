c0e7765d3fe45804a1597ee47a29ad41e61eb379
Mohebullah Yousofi <mohebullah.yousofi@gmail.com>
2025-07-26 13:39:36 +0430

    create posts and single post api

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const path = require('path');
-
+const port = process.env.PORT || 8000;
 const app = express();
 
 //setup static folder
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')));
 
+let posts = [
+   {id:1, title: 'post One'},
+   {id:2, title: 'post Two'},
+   {id:3, title: 'post Three'},
+];
 
+// Get all posts
+app.get('/api/posts', (req, res)=>{
+    res.json(posts);
+});
+
+// Get single posts
+app.get('/api/posts/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    res.json(posts.filter((post) => post.id === id));
+});
+
+/*
 app.get('/', (req, res) => {
     // res.send('Hello World');
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -16,8 +33,8 @@ app.get('/about', (req, res) => {
     // res.send('About');
     res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
+*/
 
-
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
