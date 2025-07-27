0844a77d00c3329c81f10322b0783f7387178f0d
Mohebullah Yousofi <mohebullah.yousofi@gmail.com>
2025-07-27 09:43:28 +0430

    Add delete request api

--- routes/posts.js
+++ routes/posts.js
@@ -56,6 +56,20 @@ router.put('/:id', (req, res)=>{
     post.title = req.body.title;
     res.status(200).json(posts);
 });
+
+// Delete Post
+router.delete('/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    const post = posts.find((post)=>post.id === id);
+    
+    if(!post){
+     return res.status(404).json({msg: `A post with the id of ${id} was not found`});
+    }
+
+    posts = posts.filter((post) => post.id !== id);
+    res.status(200).json(posts);
+});
+
 /*
 app.get('/', (req, res) => {
     // res.send('Hello World');
